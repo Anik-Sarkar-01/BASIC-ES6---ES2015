d2aded2f5223d13e91c79a281341f7f461f5521d
Anik-Sarkar-01 <aniksarkar8910@gmail.com>
2022-10-06 19:18:51 +0530

    problem1 solved

---
+++ problem1.js
@@ -0,0 +1,5 @@
+/*Write an arrow function that will take 3 parameters, will multiply
+the parameters and will return the result.*/
+const multiply = (num1,num2,num3) => num1*num2*num3;
+const result = multiply(10,10,10);
+console.log(result);
