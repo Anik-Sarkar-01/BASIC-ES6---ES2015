a289faa81c5b0a58b625f0af41f54a357ad98f65
Anik-Sarkar-01 <aniksarkar8910@gmail.com>
2022-10-06 19:09:52 +0530

    comments added on template string

--- template-string.js
+++ template-string.js
@@ -5,6 +5,7 @@ const person3 = `Kabul`;
 const multiLine = "First Line of code\n" + "Second Line of Code";
 console.log(multiLine);
 
+// we can make multiline easily using back tick
 const newMultiLine = `Fist Line of Code 
 Second Line of code 
 Third Line of Code`;
@@ -15,5 +16,7 @@ const b  = 30;
 const sum = " sum of: " + a + ' and ' + b + ' is: ' + (a+b);
 console.log(sum);
 
+
+// we can make strig dynamic using back tick easily.This is called string interpolation
 const newSum = `sum of ${a} and ${b} is: ${a+b}`;
 console.log(newSum);
